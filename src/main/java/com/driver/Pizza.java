4a26cd1ec284b67b90f6e2210eb4f4eb9e80db88
Anandu-Kp <anandukp06@gmail.com>
2023-01-21 10:46:39 +0530

    first submit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,33 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    int cheese;
+    int toppings;
+    boolean isCheeseAdded;
+    boolean isToppingsAdded;
+    boolean isTakeAwayAdded;
+    boolean isBillAdded;
+
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        isCheeseAdded=false;
+        isToppingsAdded=false;
+        isTakeAwayAdded=false;
+        isBillAdded=false;
+        bill="";
+       if(isVeg)
+       {
+           price=300;
+           toppings=70;
+       }
+       else
+       {
+           price=400;
+           toppings=120;
+       }
+       cheese=80;
+        bill+="Base Price Of The Pizza: "+price+"\n";
     }
 
     public int getPrice(){
@@ -17,18 +41,50 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!isCheeseAdded)
+        {
+            price+=cheese;
+            isCheeseAdded=true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!isToppingsAdded)
+        {
+            price+=toppings;
+            isToppingsAdded=true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!isTakeAwayAdded)
+        {
+            price+=20;
+            isTakeAwayAdded=true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        if(!isBillAdded)
+        {
+            isBillAdded=true;
+            if(isCheeseAdded)
+            {
+                bill+="Extra Cheese Added: "+cheese+"\n";
+            }
+            if(isToppingsAdded)
+            {
+                bill+="Extra Toppings Added: "+toppings+"\n";
+            }
+            if(isTakeAwayAdded)
+            {
+                bill+="Paperbag Added: 20\n";
+            }
+            bill+="Total Price: "+price+"\n";
+        }
         return this.bill;
     }
 }
